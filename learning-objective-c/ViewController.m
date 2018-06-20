440b162459f5ff4b8689f63aa39c8b3f3511a522
Hieu Van Phu <hieuvanphu91@gmail.com>
2018-06-20 15:42:10 +0700

    chore: clean up

--- learning-objective-c/ViewController.m
+++ learning-objective-c/ViewController.m
@@ -5,8 +5,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-}
 
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
